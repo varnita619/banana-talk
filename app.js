f5d362310babee2edd317fc34a45e1e284a5068a
Varnita <varnita.makrariya@gmail.com>
2021-07-04 16:48:31 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-var useranswer = prompt("Give me username")
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output")
 
-var welcomeMessage = "this script works!! " + useranswer;
+//  Minions Translator API
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-alert(welcomeMessage); 
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Something went wrong with the server!! Try again after sometime");
+}
+
+function getTranslatedURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+function clickEventHandler(){
+    outputDiv.innerText = txtInput.value;
+    var inputText = txtInput.value; // taking input
+
+    // Calling server for processing
+    fetch(getTranslatedURL(inputText))
+        .then(response => response.json())
+        .then(json =>{
+            var translatedText = json.contents.translated;
+        // Output 
+        outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)   
+
+    }
+
+
+btnTranslate.addEventListener("click",clickEventHandler)
